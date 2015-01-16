45eef935b01abbd2c67b3d02814d3407db691f14
Nicolas Widart <n.widart@gmail.com>
2015-01-16 20:38:16 +0100

    Remove unused imports

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -1,24 +1,9 @@
 <?php
 
-use Modules\Acme\Commands\ExampleCommandHandler;
-
 class HomeController extends BaseController
 {
-    /**
-     * @var ExampleCommandHandler
-     */
-    private $exampleCommandHandler;
-
-    public function __construct(ExampleCommandHandler $exampleCommandHandler)
-    {
-        parent::__construct();
-
-        $this->exampleCommandHandler = $exampleCommandHandler;
-    }
-
     public function showWelcome()
     {
-        $this->exampleCommandHandler->testing();
         return View::make('hello');
     }
 }
--- app/routes.php
+++ app/routes.php
@@ -1,5 +1,6 @@
 <?php
 
-Route::get('/', 'Modules\Acme\Controllers\HomeController@index');
+Route::get('/', 'HomeController@showWelcome');
+Route::get('/command-handling', 'Modules\Acme\Controllers\HomeController@index');
 Route::get('/event-dispatcher', 'Modules\Acme\Controllers\HomeController@eventDispatcher');
 Route::get('/event-handling', 'Modules\Acme\Controllers\HomeController@eventHandling');
