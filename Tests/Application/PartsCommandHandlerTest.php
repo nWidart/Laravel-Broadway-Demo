0d35a1b28176e96f7caf2ba383989ea3e7a7f5fc
Nicolas Widart <n.widart@gmail.com>
2015-01-16 20:29:48 +0100

    Adding parts command handler test

---
+++ Tests/Application/PartsCommandHandlerTest.php
@@ -0,0 +1,83 @@
+<?php namespace Asgard\Application;
+
+use Broadway\CommandHandling\CommandHandlerInterface;
+use Broadway\CommandHandling\Testing\CommandHandlerScenarioTestCase;
+use Broadway\EventHandling\EventBusInterface;
+use Broadway\EventStore\EventStoreInterface;
+use Broadway\UuidGenerator\Rfc4122\Version4Generator;
+use Modules\Parts\Commands\Handlers\PartCommandHandler;
+use Modules\Parts\Commands\ManufacturePartCommand;
+use Modules\Parts\Commands\RenameManufacturerForPartCommand;
+use Modules\Parts\Events\PartManufacturerWasRenamedEvent;
+use Modules\Parts\Events\PartWasManufacturedEvent;
+use Modules\Parts\Repositories\PartRepository;
+
+class PartsCommandHandlerTest extends CommandHandlerScenarioTestCase
+{
+    /**
+     * @var Version4Generator
+     */
+    private $generator;
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->generator = new Version4Generator();
+    }
+
+    /**
+     * Create a command handler for the given scenario test case.
+     *
+     * @param EventStoreInterface $eventStore
+     * @param EventBusInterface $eventBus
+     * @return CommandHandlerInterface
+     */
+    protected function createCommandHandler(EventStoreInterface $eventStore, EventBusInterface $eventBus)
+    {
+        $repository = new PartRepository($eventStore, $eventBus);
+
+        return new PartCommandHandler($repository);
+    }
+
+    /**
+     * @test
+     */
+    public function it_can_manufacture()
+    {
+        $id = $this->generator->generate();
+        $this->scenario
+            ->withAggregateId($id)
+            ->given([])
+            ->when(new ManufacturePartCommand($id, 'acme', 'Acme, Inc'))
+            ->then([new PartWasManufacturedEvent($id, 'acme', 'Acme, Inc')]);
+    }
+
+    /**
+     * @test
+     */
+    public function it_can_rename_manufacturer()
+    {
+        $id = $this->generator->generate();
+        $this->scenario
+            ->withAggregateId($id)
+            ->given([new PartWasManufacturedEvent($id, 'acme', 'Acme, Inc')])
+            ->when(new RenameManufacturerForPartCommand($id, 'Acme, Inc.'))
+            ->then([new PartManufacturerWasRenamedEvent($id, 'Acme, Inc.')]);
+    }
+
+    /**
+     * @test
+     */
+    public function it_does_not_rename_manufacturer_to_the_same_name()
+    {
+        $id = $this->generator->generate();
+        $this->scenario
+            ->withAggregateId($id)
+            ->given([
+                new PartWasManufacturedEvent($id, 'acme', 'Acme, Inc'),
+                new PartWasManufacturedEvent($id, 'acme', 'Acme, Inc.'),
+            ])
+            ->when(new RenameManufacturerForPartCommand($id, 'Acme, Inc.'))
+            ->then([]);
+    }
+}
