0ea57c1b557f4aeae736daac4e584b4816370a33
Nicolas Widart <n.widart@gmail.com>
2015-01-16 20:28:48 +0100

    Use the Part class name

--- Modules/Parts/Repositories/PartRepository.php
+++ Modules/Parts/Repositories/PartRepository.php
@@ -4,11 +4,12 @@ use Broadway\EventHandling\EventBusInterface;
 use Broadway\EventSourcing\AggregateFactory\PublicConstructorAggregateFactory;
 use Broadway\EventSourcing\EventSourcingRepository;
 use Broadway\EventStore\EventStoreInterface;
+use Modules\Parts\Entities\Part;
 
 class PartRepository extends EventSourcingRepository
 {
     public function __construct(EventStoreInterface $eventStore, EventBusInterface $eventBus)
     {
-        parent::__construct($eventStore, $eventBus, 'Part', new PublicConstructorAggregateFactory());
+        parent::__construct($eventStore, $eventBus, Part::class, new PublicConstructorAggregateFactory());
     }
 }
