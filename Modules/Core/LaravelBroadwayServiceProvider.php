f35429cb881867d3c0dc2186816f08bd462538fb
Nicolas Widart <n.widart@gmail.com>
2015-01-17 10:14:14 +0100

    Adding a basic service

---
+++ Modules/Core/LaravelBroadwayServiceProvider.php
@@ -0,0 +1,15 @@
+<?php namespace Modules\Core;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelBroadwayServiceProvider extends ServiceProvider
+{
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+    }
+}
--- app/config/app.php
+++ app/config/app.php
@@ -121,7 +121,8 @@ return array(
 		'Illuminate\Validation\ValidationServiceProvider',
 		'Illuminate\View\ViewServiceProvider',
 		'Illuminate\Workbench\WorkbenchServiceProvider',
-        'Mitch\LaravelDoctrine\LaravelDoctrineServiceProvider'
+        'Mitch\LaravelDoctrine\LaravelDoctrineServiceProvider',
+        'Modules\Core\LaravelBroadwayServiceProvider',
 	),
 
 	/*
