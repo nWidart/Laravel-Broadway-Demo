e7aa745e1851f138bea2160249f0ac8729670935
Nicolas Widart <n.widart@gmail.com>
2015-01-17 10:33:48 +0100

    Make a basic manufacture controller method

--- Modules/Parts/Controllers/PartsController.php
+++ Modules/Parts/Controllers/PartsController.php
@@ -1,14 +1,33 @@
 <?php namespace Modules\Parts\Controllers;
 
+use Broadway\CommandHandling\CommandBusInterface;
 use Modules\Parts\Commands\Handlers\PartCommandHandler;
+use Modules\Parts\Commands\ManufacturePartCommand;
+use Modules\Parts\Entities\ManufacturerId;
+use Modules\Parts\Entities\PartId;
 use Modules\Parts\Repositories\PartRepository;
 
 class PartsController extends \BaseController
 {
+    /**
+     * @var CommandBusInterface
+     */
+    private $commandBus;
+
+    public function __construct(CommandBusInterface $commandBus)
+    {
+        $this->commandBus = $commandBus;
+    }
+
     public function manufacture()
     {
-        dd('ok');
-        // Setup command handler
-        //$commandHandler = new PartCommandHandler(new PartRepository());
+        $partId = PartId::generate();
+        $manufacturerId = ManufacturerId::generate();
+
+        $command = new ManufacturePartCommand($partId, $manufacturerId, 'BWM');
+
+        $this->commandBus->dispatch($command);
+
+        dd('Something happened ?');
     }
 }
