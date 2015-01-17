08d955c33336906baebb5886d88b59eb1a8186e6
Nicolas Widart <n.widart@gmail.com>
2015-01-17 20:48:11 +0100

    Implementing the serializable interface

--- Modules/Parts/Events/PartManufacturerWasRenamedEvent.php
+++ Modules/Parts/Events/PartManufacturerWasRenamedEvent.php
@@ -1,8 +1,9 @@
 <?php namespace Modules\Parts\Events;
 
+use Broadway\Serializer\SerializableInterface;
 use Modules\Parts\Entities\PartId;
 
-class PartManufacturerWasRenamedEvent
+class PartManufacturerWasRenamedEvent implements SerializableInterface
 {
     public $partId;
     public $manufacturerName;
@@ -11,5 +12,24 @@ class PartManufacturerWasRenamedEvent
     {
         $this->partId = $partId;
         $this->manufacturerName = $manufacturerName;
+    }
+
+    /**
+     * @return mixed The object instance
+     */
+    public static function deserialize(array $data)
+    {
+        return new self($data['partId'], $data['manufacturerName']);
+    }
+
+    /**
+     * @return array
+     */
+    public function serialize()
+    {
+        return [
+            'partId' => $this->partId,
+            'manufacturerName' => $this->manufacturerName,
+        ];
     }
 }
