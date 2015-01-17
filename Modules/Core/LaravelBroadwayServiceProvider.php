ffb536fda31d6992380c73154c6f774fa4de8c1b
Nicolas Widart <n.widart@gmail.com>
2015-01-17 16:10:13 +0100

    Reorganising the service provider

--- Modules/Core/LaravelBroadwayServiceProvider.php
+++ Modules/Core/LaravelBroadwayServiceProvider.php
@@ -6,6 +6,8 @@ use Broadway\EventHandling\SimpleEventBus;
 use Broadway\EventStore\DBALEventStore;
 use Broadway\EventStore\InMemoryEventStore;
 use Broadway\UuidGenerator\Rfc4122\Version4Generator;
+use Doctrine\DBAL\Configuration;
+use Doctrine\DBAL\DriverManager;
 use Illuminate\Support\ServiceProvider;
 use Modules\Parts\Repositories\EventStorePartRepository;
 
@@ -18,39 +20,55 @@ class LaravelBroadwayServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        // Bind a CommandBus
-        $this->app->bindShared('Broadway\CommandHandling\CommandBusInterface', function () {
-            return new SimpleCommandBus();
-        });
-
-        // Bind a Uui Generator
-        $this->app->bindShared('Broadway\UuidGenerator\UuidGeneratorInterface', function () {
-            return new Version4Generator();
-        });
-
-        // Bind an event dispatcher
-        $this->app->bindShared('Broadway\EventDispatcher\EventDispatcherInterface', function () {
-            return new EventDispatcher();
-        });
-
+        $this->bindEventClasses();
         $this->bindEventStorage();
+        $this->bindEventSourcedRepositories();
+        $this->bindMiscClasses();
     }
 
     private function bindEventStorage()
     {
         $this->app->bind('Broadway\EventStore\EventStoreInterface', function () {
+            $dbalConf = new Configuration();
+            $con = DriverManager::getConnection(['driver' => 'pdo_mysql'], $dbalConf);
+            //dd($con);
+            //$dbalEventStore = new DBALEventStore($con);
             return new InMemoryEventStore(); # Temporary Needs Broadway\EventStore\DBALEventStore
         });
 
         $this->app->bind('Broadway\EventHandling\EventBusInterface', function () {
             return new SimpleEventBus();
         });
+    }
 
+    private function bindEventSourcedRepositories()
+    {
         // Binding the Part Repository
         $this->app->bind('Modules\Parts\Repositories\PartRepository', function ($app) {
             $eventStore = $app['Broadway\EventStore\EventStoreInterface'];
             $eventBus = $app['Broadway\EventHandling\EventBusInterface'];
             return new EventStorePartRepository($eventStore, $eventBus);
+        });
+    }
+
+    private function bindEventClasses()
+    {
+        // Bind an event dispatcher
+        $this->app->bind('Broadway\EventDispatcher\EventDispatcherInterface', function () {
+            return new EventDispatcher();
+        });
+
+        // Bind a CommandBus
+        $this->app->bind('Broadway\CommandHandling\CommandBusInterface', function () {
+            return new SimpleCommandBus();
+        });
+    }
+
+    private function bindMiscClasses()
+    {
+        // Bind a Uui Generator
+        $this->app->bind('Broadway\UuidGenerator\UuidGeneratorInterface', function () {
+            return new Version4Generator();
         });
     }
 }
