6f81f5431c0d3df41a6ccf89e5c09a4e5de9bba4
Nicolas Widart <n.widart@gmail.com>
2015-01-18 21:10:12 +0100

    Implementing read model interface and serializable

--- Modules/Parts/ReadModel/PartsThatWereManufactured.php
+++ Modules/Parts/ReadModel/PartsThatWereManufactured.php
@@ -1,6 +1,39 @@
 <?php namespace Modules\Parts\ReadModel;
 
-class PartsThatWereManufactured
-{
+use Broadway\ReadModel\ReadModelInterface;
+use Broadway\Serializer\SerializableInterface;
 
+class PartsThatWereManufactured implements ReadModelInterface, SerializableInterface
+{
+    private $manufacturedPartId;
+
+    public function __construct($manufacturedPartId)
+    {
+        $this->manufacturedPartId = $manufacturedPartId;
+    }
+
+    /**
+     * @return string
+     */
+    public function getId()
+    {
+        return $this->manufacturedPartId;
+    }
+
+    /**
+     * @param array $data
+     * @return mixed The object instance
+     */
+    public static function deserialize(array $data)
+    {
+        return new self($data['manufacturedPartId']);
+    }
+
+    /**
+     * @return array
+     */
+    public function serialize()
+    {
+        return ['manufacturedPartId' => $this->manufacturedPartId];
+    }
 }
