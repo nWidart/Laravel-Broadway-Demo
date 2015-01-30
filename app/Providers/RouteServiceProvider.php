2fb26a637faca874118722cca1af5eac96af4a19
Nicolas Widart <n.widart@gmail.com>
2015-01-30 14:25:24 +0100

    Use the Module namespace

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -12,7 +12,7 @@ class RouteServiceProvider extends ServiceProvider {
 	 *
 	 * @var string
 	 */
-	protected $namespace = 'App\Http\Controllers';
+	protected $namespace = 'Modules';
 
 	/**
 	 * Define your route model bindings, pattern filters, etc.
