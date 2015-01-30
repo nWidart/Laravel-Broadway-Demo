ddb147f561b3ea81a983d6c02e2e5ffece69803b
Nicolas Widart <n.widart@gmail.com>
2015-01-30 14:25:39 +0100

    Place the routes back

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,21 +1,16 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
-
 Route::get('/', 'WelcomeController@index');
 
-Route::get('home', 'HomeController@index');
+Route::get('/command-handling', 'Acme\Controllers\HomeController@index');
+Route::get('/event-dispatcher', 'Acme\Controllers\HomeController@eventDispatcher');
+Route::get('/event-handling', 'Acme\Controllers\HomeController@eventHandling');
 
-Route::controllers([
-	'auth' => 'Auth\AuthController',
-	'password' => 'Auth\PasswordController',
-]);
+/*
+|--------------------------------------------------------------------------
+| Event sourced child entity
+|--------------------------------------------------------------------------
+*/
+Route::get('/parts/manufacture', 'Parts\Controllers\PartsController@manufacture');
+Route::get('/parts/manufactured-parts', 'Parts\Controllers\PartsController@manufacturedParts');
+Route::get('/parts/rename', 'Parts\Controllers\PartsController@renameManufacturer');
