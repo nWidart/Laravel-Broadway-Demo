6d4f5f65a939442aa36f383ec7bd41db2d51b8f8
Nicolas Widart <n.widart@gmail.com>
2015-01-30 20:18:01 +0100

    Refactor the getReadModel

--- Modules/Parts/ReadModel/PartsThatWereManufacturedProjector.php
+++ Modules/Parts/ReadModel/PartsThatWereManufacturedProjector.php
@@ -1,5 +1,6 @@
 <?php namespace Modules\Parts\ReadModel;
 
+use Assert\Assertion;
 use Broadway\ReadModel\Projector;
 use Modules\Parts\Events\PartManufacturerWasRenamedEvent;
 use Modules\Parts\Events\PartWasManufacturedEvent;
@@ -19,28 +20,30 @@ class PartsThatWereManufacturedProjector extends Projector
 
     public function applyPartWasManufacturedEvent(PartWasManufacturedEvent $event)
     {
-        $readModel = $this->getReadModel($event->partId, $event->manufacturerName);
+        try {
+            $readModel = $this->getReadModel($event->partId, $event->manufacturerName);
+        } catch (\Exception $e) {
+            $readModel = new PartsThatWereManufactured($event->partId->toString(), $event->manufacturerName);
+        }
 
         $this->repository->save($readModel);
     }
 
     public function applyPartManufacturerWasRenamedEvent(PartManufacturerWasRenamedEvent $event)
     {
-        $readModel = $this->getReadModel($event->partId, $event->manufacturerName);
+        $readModel = $this->getReadModel($event->partId);
 
         $readModel->renameManufacturer($event->manufacturerName);
 
         $this->repository->save($readModel);
     }
 
-    public function getReadModel($partId, $manufacturerName)
+    public function getReadModel($partId)
     {
         $partId = (string) $partId;
         $readModel = $this->repository->find($partId);
 
-        if (null === $readModel) {
-            $readModel = new PartsThatWereManufactured($partId, $manufacturerName);
-        }
+        Assertion::isInstanceOf($readModel, PartsThatWereManufactured::class);
 
         return $readModel;
     }
