0ffa69652752e4a9ceff51b5b699dc704c49cbe3
Nicolas Widart <n.widart@gmail.com>
2015-02-03 12:25:08 +0100

    Publishing the broadway config file

---
+++ config/broadway.php
@@ -0,0 +1,33 @@
+<?php
+
+return [
+    /*
+    |--------------------------------------------------------------------------
+    | Event Store configuration
+    |--------------------------------------------------------------------------
+    | Set the table name where the event will be stored. Make sure
+    | this corresponds to what you specified in the migration
+    |
+    | You should also choose a driver, possible options are
+    | dbal, inmemory
+    */
+    'event-store' => [
+        'table' => 'event_store',
+        'driver' => 'dbal',
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Choose which read model implementation to use
+    | Possible options are: elasticsearch, inmemory
+    |--------------------------------------------------------------------------
+    */
+    'read-model' => 'elasticsearch',
+    'read-model-connections' => [
+        'elasticsearch' => [
+            'config' => [
+                'hosts' => ['localhost:9200'],
+            ],
+        ],
+    ],
+];
