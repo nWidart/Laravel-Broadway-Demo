f1d772ba4d2ec157cc86ce1acf0f96291f415b75
Nicolas Widart <n.widart@gmail.com>
2015-02-03 12:24:56 +0100

    Adding event store migration file

---
+++ database/migrations/2015_01_20_181911_create_event_store_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEventStoreTable extends Migration
+{
+    /**
+     * @var string
+     */
+    private $eventStoreTableName;
+
+    public function __construct()
+    {
+        $this->eventStoreTableName = Config::get('broadway.event-store-table', 'event_store');
+    }
+
+    public function up()
+    {
+        Schema::create($this->eventStoreTableName, function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->string('uuid', 36);
+            $table->integer('playhead')->unsigned();
+            $table->text('metadata');
+            $table->text('payload');
+            $table->string('recorded_on', 32);
+            $table->text('type');
+
+            $table->unique(['uuid', 'playhead']);
+        });
+    }
+
+    public function down()
+    {
+        Schema::drop($this->eventStoreTableName);
+    }
+}
