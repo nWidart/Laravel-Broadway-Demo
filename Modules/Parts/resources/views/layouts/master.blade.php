1ef733ebe05a75412f59df3f89ebf069eaa40173
Nicolas Widart <n.widart@gmail.com>
2015-02-04 09:54:05 +0100

    Add a scripts section

--- Modules/Parts/resources/views/layouts/master.blade.php
+++ Modules/Parts/resources/views/layouts/master.blade.php
@@ -18,6 +18,9 @@
             </div>
         </div>
 
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
         <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+        @section('scripts')
+        @show
     </body>
 </html>
