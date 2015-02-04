bb38e2d23722693ccd2e3f4e16051cd19b7345c6
Nicolas Widart <n.widart@gmail.com>
2015-02-04 09:33:32 +0100

    Adding FA icons

--- Modules/Parts/resources/views/partials/sidebar.blade.php
+++ Modules/Parts/resources/views/partials/sidebar.blade.php
@@ -1,6 +1,6 @@
 <div class="col-sm-3 col-md-2 sidebar">
     <ul class="nav nav-sidebar">
-        <li class="active"><a href="#">Overview <span class="sr-only">(current)</span></a></li>
-        <li><a href="#">Parts</a></li>
+        <li class="active"><a href="#"><i class="fa fa-tachometer"></i> Overview <span class="sr-only">(current)</span></a></li>
+        <li><a href="#"><i class="fa fa-cubes"></i> Parts</a></li>
     </ul>
 </div>
