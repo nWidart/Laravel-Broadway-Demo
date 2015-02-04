fc687e577d983be95b98fee7363c0d10b51c3193
Nicolas Widart <n.widart@gmail.com>
2015-02-04 14:57:40 +0100

    Adding a _token to ajax calls

--- Modules/Parts/resources/views/layouts/master.blade.php
+++ Modules/Parts/resources/views/layouts/master.blade.php
@@ -22,6 +22,15 @@
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
         <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
         <script src="{{ Module::asset('parts:js/vendor/bootstrap-editable.min.js') }}"></script>
+        <script>
+            $( document ).ready(function() {
+                $.ajaxSetup({
+                    data: {
+                        _token: '<?= csrf_token() ?>'
+                    }
+                });
+            });
+        </script>
         @section('scripts')
         @show
     </body>
