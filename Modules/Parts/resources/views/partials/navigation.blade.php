5cdff4d35836f5f149c55e7da08768ab87fc9ec2
Nicolas Widart <n.widart@gmail.com>
2015-02-04 09:34:21 +0100

    Removing main navbar link

--- Modules/Parts/resources/views/partials/navigation.blade.php
+++ Modules/Parts/resources/views/partials/navigation.blade.php
@@ -11,7 +11,6 @@
         </div>
         <div id="navbar" class="navbar-collapse collapse">
             <ul class="nav navbar-nav navbar-right">
-                <li><a href="#">Dashboard</a></li>
             </ul>
         </div>
     </div>
