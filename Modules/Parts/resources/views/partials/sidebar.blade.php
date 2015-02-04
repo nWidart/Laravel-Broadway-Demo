6b1135f1dc22234fbc5b3e514b3eb027a51b1cd0
Nicolas Widart <n.widart@gmail.com>
2015-02-04 09:43:34 +0100

    Add active link on parts

--- Modules/Parts/resources/views/partials/sidebar.blade.php
+++ Modules/Parts/resources/views/partials/sidebar.blade.php
@@ -1,6 +1,6 @@
 <div class="col-sm-3 col-md-2 sidebar">
     <ul class="nav nav-sidebar">
-        <li class="active"><a href="#"><i class="fa fa-tachometer"></i> Overview <span class="sr-only">(current)</span></a></li>
-        <li><a href="#"><i class="fa fa-cubes"></i> Parts</a></li>
+        <li><a href="#"><i class="fa fa-tachometer"></i> Overview <span class="sr-only">(current)</span></a></li>
+        <li class="{{ Request::is('parts/*') ? 'active' : '' }}"><a href="#"><i class="fa fa-cubes"></i> Parts</a></li>
     </ul>
 </div>
