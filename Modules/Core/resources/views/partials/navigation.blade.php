2b2e10a7d952f1558c474e88f64deea42ebc8b0d
Nicolas Widart <n.widart@gmail.com>
2015-02-07 18:34:25 +0100

    Adding link on logo

--- Modules/Core/resources/views/partials/navigation.blade.php
+++ Modules/Core/resources/views/partials/navigation.blade.php
@@ -7,7 +7,7 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="#">Event Sourcing and CQRS Demo</a>
+            <a class="navbar-brand" href="{{ route('dashboard.index') }}">Event Sourcing and CQRS Demo</a>
         </div>
         <div id="navbar" class="navbar-collapse collapse">
             <ul class="nav navbar-nav navbar-right">
